b78fd8710140ec3820f0c55d59875b0ea84344d0
Omar Pérez <aomarperez17@gmail.com>
2021-09-02 20:07:12 -0600

    Se agregaron los admins respectivos a cada Módulo, y el registro para el admin

--- app/core/descuento/admin.py
+++ app/core/descuento/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from core.descuento.models import Descuento
 
-# Register your models here.
+
+class DescuentoAdmin(admin.ModelAdmin):
+    list_display = ['cod_descuento','porcentaje','descripcion']
+    list_filter = ['porcentaje']
+    list_editable = ['porcentaje','descripcion']
+    list_per_page = 10
+    search_fields = ['descripcion']
+
+
+admin.site.register(Descuento,DescuentoAdmin)
--- app/core/sucursal/admin.py
+++ app/core/sucursal/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from core.sucursal.models import Sucursal,UsuarioSucursal,PermisoUsuarioSucursal,Permiso
 
-# Register your models here.
+
+class SucursalAdmin(admin.ModelAdmin):
+    list_display = ['id_sucursal','nombre','correo','direccion','telefono_num']
+    list_filter = []
+    list_editable = ['nombre','correo','direccion','telefono_num']
+    list_per_page = 10
+    search_fields = ['nombre','correo','telefono_num']
+
+
+class UsuarioSucursalAdmin(admin.ModelAdmin):
+    list_display = ['usuario_sucursal','pass_field','activo','fecha_creado','sucursal']
+    list_filter = ['activo','sucursal','fecha_creado']
+    list_editable = ['pass_field','activo']
+    list_per_page = 10
+    search_fields = ['sucursal']
+
+
+class PermisoAdmin(admin.ModelAdmin):
+    list_display = ['permiso']
+    list_filter = []
+    list_editable = []
+    list_per_page = 10
+    search_fields = ['permiso']
+
+
+class PermisoUsuarioSucursalAdmin(admin.ModelAdmin):
+    list_display = ['id_permiso_usuario_sucursal','sucursal_usuario','permiso']
+    list_filter = ['permiso']
+    list_editable = ['sucursal_usuario','permiso']
+    list_per_page = 10
+    search_fields = ['sucursal_usuario','permiso']
+
+
+admin.site.register(Sucursal,SucursalAdmin)
+admin.site.register(UsuarioSucursal,UsuarioSucursalAdmin)
+admin.site.register(Permiso,PermisoAdmin)
+admin.site.register(PermisoUsuarioSucursal,PermisoUsuarioSucursalAdmin)
